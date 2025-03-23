59d42c1e0af88572010f1877853532f4356f4342
levon <levon>
2025-03-23 14:45:51 +0400

    Simpl hot reloader is ready

---
+++ HotReload.java
@@ -0,0 +1,95 @@
+import java.io.IOException;
+import java.nio.file.*;
+
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.TimeUnit;
+import java.util.*;
+
+public class HotReload {
+    public static final Long TIME_TO_COMPILE = 500L;
+    public static final Map<Path, Long> lastTimeChanges = new HashMap<>();
+    
+    public static boolean isTimeToComple(Path file) {
+        long currentTime = System.currentTimeMillis();
+        long lastTimeChange = lastTimeChanges.getOrDefault(file, 0L);
+        
+        if (currentTime - lastTimeChange >= TIME_TO_COMPILE) {
+            lastTimeChanges.put(file, currentTime);
+            return true;
+        }
+        return false;
+    }
+
+    public static void registerAll(WatchService watchService,
+            Map<WatchKey, Path> keyMap, Path start) throws IOException {
+        Files.walkFileTree(start, new SimpleFileVisitor<Path>() {
+           public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+               WatchKey key = dir.register(watchService,
+                       StandardWatchEventKinds.ENTRY_CREATE,
+                       StandardWatchEventKinds.ENTRY_MODIFY);
+               keyMap.put(key, dir);
+               System.out.println("[INFO]:📂 Watching directory: " + dir);
+
+               return FileVisitResult.CONTINUE;
+           } 
+        });
+    }
+
+    public static void compileProgram() {
+        try {
+            System.out.println("[INFO]:⚡ Running maven compiler...");        
+            Process process = new ProcessBuilder("mvn", "compile", "-T", "1C")
+                .inheritIO()
+                .start();
+                process.waitFor();
+                System.out.println("[INFO]: Compilation complete, waiting for new changes!! ");         
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            throw new Exception("Please provide path for scanning");
+        }
+
+        Path watchingPath = Paths.get(args[0]);
+
+        try(WatchService watchService = FileSystems.getDefault().newWatchService()) {
+            Map<WatchKey, Path> keyMap = new HashMap<>();
+            registerAll(watchService, keyMap, watchingPath);
+
+            System.out.println("[INFO]: Scanning " + watchingPath.toString() + " for file changes");
+
+            while (true) {
+                WatchKey key = watchService.poll(10, TimeUnit.SECONDS);
+                Path dir = keyMap.get(key);
+                if (dir != null) {
+                    for (WatchEvent<?> event : key.pollEvents()) {
+                        WatchEvent.Kind<?> kind = event.kind();
+                        Path changedFile = dir.resolve((Path) event.context()); 
+
+                        if (kind == StandardWatchEventKinds.ENTRY_MODIFY && changedFile.toString().endsWith(".java")) {
+                            if (isTimeToComple(changedFile)) {
+                                System.out.println("[INFO]:" + "File changed " + event.context());            
+                                compileProgram();
+                            }
+                        }
+                        else if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
+                            if (Files.isDirectory(changedFile)) {
+                                System.out.println("[INFO]:📂 Created new directory : " + changedFile);
+                                registerAll(watchService, keyMap, changedFile);
+                            }
+                        }
+                    }
+                    key.reset();
+                }
+            }
+        } catch(IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# HotReloader
+## Simple java program which can help 
+## to detect changes in your project
+## and compile your program 
+> (Note it works only with maven project)
+# Simple use
+```
+    javac HotReload.java
+    java HotReload <path to directory to scan>
+``` 
